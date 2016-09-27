8123d620485b14cde3f762a2803bfc0c906c83db
Huang, Leon <liang.huang@hpe.com>
2016-09-27 06:27:06 +0000

    change port configuration for es and rename to es2 to avoid conflicts.

--- kibana/entrypoint.sh
+++ kibana/entrypoint.sh
@@ -3,7 +3,7 @@
 # Wait for the Elasticsearch container to be ready before starting Kibana.
 echo "Stalling for Elasticsearch"
 while true; do
-    nc -q 1 elasticsearch 9200 2>/dev/null && break
+    nc -q 1 elasticsearch2 9200 2>/dev/null && break
 done
 
 echo "Starting Kibana"
